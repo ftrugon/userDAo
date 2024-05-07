2b4697caeb6eb35237747eecde70586f587cc7e5
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-05-07 19:48:18 +0200

    joder

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,49 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import dao.UserDAOH2
+import entity.UserEntity
+import service.UserServiceImpl
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // Creamos la instancia de la base de datos
+    val dataSource = DataSourceFactory.getDS(DataSourceFactory.DataSourceType.HIKARI)
+
+    // Creamos la instancia de UserDAO
+    val userDao = UserDAOH2(dataSource)
+
+    // Creamos la instancia de UserService
+    val userService = UserServiceImpl(userDao)
+
+    // Creamos un nuevo usuario
+    val newUser = UserEntity(name = "John Doe", email = "johndoe@example.com")
+    var createdUser = userService.create(newUser)
+    println("Created user: $createdUser")
+
+    // Obtenemos un usuario por su ID
+    val foundUser = userService.getById(createdUser.id)
+    println("Found user: $foundUser")
+
+    // Actualizamos el usuario
+    val updatedUser = foundUser!!.copy(name = "Jane Doe")
+    val savedUser = userService.update(updatedUser)
+    println("Updated user: $savedUser")
+
+    val otherUser = UserEntity(name = "Eduardo Fernandez", email = "eferoli@gmail.com")
+    createdUser = userService.create( otherUser)
+    println("Created user: $createdUser")
+
+
+    // Obtenemos todos los usuarios
+    var allUsers = userService.getAll()
+    println("All users: $allUsers")
+
+    // Eliminamos el usuario
+    userService.delete(savedUser.id)
+    println("User deleted")
+
+    // Obtenemos todos los usuarios
+    allUsers = userService.getAll()
+    println("All users: $allUsers")
+
+    // Eliminamos el usuario
+    userService.delete(otherUser.id)
+    println("User deleted")
 }
---
+++ src/main/kotlin/service/UserServiceImpl.kt
@@ -0,0 +1,27 @@
+package service
+
+import dao.UserDAOH2
+import entity.UserEntity
+import java.util.*
+
+class UserServiceImpl(private val userDao: UserDAOH2) : IUserService {
+    override fun create(user: UserEntity): UserEntity {
+        return userDao.create(user)
+    }
+
+    override fun getById(id: UUID): UserEntity? {
+        return userDao.getById(id)
+    }
+
+    override fun update(user: UserEntity): UserEntity {
+        return userDao.update(user)
+    }
+
+    override fun delete(id: UUID) {
+        userDao.delete(id)
+    }
+
+    override fun getAll(): List<UserEntity> {
+        return userDao.getAll()
+    }
+}
